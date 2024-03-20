ed78c64154c67f1da85db624293684070fce2ec3
Toba <adesanyatoba@gmail.com>
2024-03-20 10:19:26 +0000

    Add new views

--- web_app/hello/views.py
+++ web_app/hello/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world!")
+
+
+def bruce(request):
+    return HttpResponse("Hello, Bruce!")
