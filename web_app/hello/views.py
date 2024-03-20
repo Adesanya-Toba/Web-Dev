a2a0a085412eecaa9cbca04f71aa4a80b3b56179
Toba <adesanyatoba@gmail.com>
2024-03-20 11:55:17 +0000

    Add template html with django

--- web_app/hello/views.py
+++ web_app/hello/views.py
@@ -3,13 +3,19 @@ from django.http import HttpResponse
 
 
 # Create your views here.
-def index(request):
+def index(request) -> HttpResponse:
     return render(request, "hello/index.html")
 
 
-def bruce(request):
+def bruce(request) -> HttpResponse:
     return HttpResponse("Hello, Bruce!")
 
 
-def greet(request, name: str):
-    return HttpResponse(f"Hello, {name.capitalize()}!")
+def greet(request, name: str) -> HttpResponse:
+    return render(
+        request,
+        template_name="hello/greet.html",
+        context={
+            "name": name.capitalize()
+        },  # Context argument to provide additional information to an HTML template
+    )
