040349a1ff910f3eea65dd3bc9d6a7117a75dee5
Toba-Adesanya <adesanyatoba@gmail.com>
2024-06-08 19:28:11 +0100

    First Dash app

---
+++ Dash_app/First_app/app.py
@@ -0,0 +1,32 @@
+from dash import (
+    Dash,
+    html,
+    dcc,
+    callback,
+    Output,
+    Input,
+)
+import plotly.express as px
+import pandas as pd
+
+data_frame = pd.read_csv(
+    "https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv"
+)
+
+app = Dash()
+
+app.layout = [
+    html.H1(children="First Dash App", style={"textAlign": "center"}),
+    dcc.Dropdown(data_frame.country.unique(), "Canada", id="dropdown-selection"),
+    dcc.Graph(id="graph-content"),
+]
+
+
+@callback(Output("graph-content", "figure"), Input("dropdown-selection", "value"))
+def update_graph(value):
+    dff = data_frame[data_frame.country == value]
+    return px.line(dff, x="year", y="pop")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
