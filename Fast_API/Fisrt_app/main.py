d1c3d2c3d30a78042245fa8529d0890453c495e3
Toba-Adesanya <adesanyatoba@gmail.com>
2024-06-08 18:25:18 +0100

    Add more http routes

--- Fast_API/Fisrt_app/main.py
+++ Fast_API/Fisrt_app/main.py
@@ -22,7 +22,7 @@ class Item(BaseModel):
     category: Category
 
 
-items = {
+items: dict[int, Item] = {
     0: Item(name="Hammer", price=9.89, count=20, id=0, category=Category.TOOLS),
     1: Item(name="Pliers", price=5.89, count=20, id=1, category=Category.TOOLS),
     2: Item(name="Nails", price=1.99, count=100, id=2, category=Category.CONSUMABLES),
@@ -90,3 +90,48 @@ def query_item_by_parameters(
             "selection": selection,
         }
     }
+
+
+@app.post("/")
+def add_item(item: Item) -> dict[str, Item]:
+    if item.id in items:
+        HTTPException(status_code=400, detail=f"Item with {item.id=} already exists!")
+
+    items[item.id] = item
+    return {"added": item}
+
+
+@app.put("/update/{item_id}")
+def update(
+    item_id: int,
+    name: str | None = None,
+    price: float | None = None,
+    count: int | None = None,
+) -> dict[str, Item]:
+    if item_id not in items:
+        HTTPException(status_code=404, detail=f"Item with {item_id=} does not exist!")
+    if all(info is None for info in (name, price, count)):
+        raise HTTPException(
+            status_code=400, detail="No parameters provided for update."
+        )
+
+    item = items[item_id]
+    if name is not None:
+        item.name = name
+    if price is not None:
+        item.price = price
+    if count is not None:
+        item.count = count
+
+    return {"updated": item}
+
+
+@app.delete("/delete/{item_id}")
+def delete_item(item_id: int) -> dict[str, Item]:
+    if item_id not in items:
+        raise HTTPException(
+            status_code=404, detail=f"Item with {item_id=} does not exist!"
+        )
+
+    item = items.pop(item_id)
+    return {"deleted": item}
