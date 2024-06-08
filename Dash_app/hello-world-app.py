67685dcbbe97fa5cd61ffbb21a0198afbc6513ec
Toba-Adesanya <adesanyatoba@gmail.com>
2024-06-08 19:42:03 +0100

    Add line descriptions

--- Dash_app/hello-world-app.py
+++ Dash_app/hello-world-app.py
@@ -1,8 +1,12 @@
 from dash import Dash, html
 
+# Initialize the dash app
 app = Dash()
 
+# The app layout represents the app components that will be
+# displayed on the web browser, shown here as a list.
 app.layout = [html.Div(children="Hello World")]
 
+# Run the app!
 if __name__ == "__main__":
     app.run(debug=True)
