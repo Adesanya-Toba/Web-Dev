8f435b1bfd11efa21cedd557e65def82c3222e55
Toba-Adesanya <adesanyatoba@gmail.com>
2024-07-28 22:34:53 +0100

    Working with other props and  arrays in react

--- React-TS/react-typescript-demo/src/App.tsx
+++ React-TS/react-typescript-demo/src/App.tsx
@@ -1,10 +1,33 @@
 import './App.css';
 import { Greet } from './components/Greet';
+import { Person } from './components/Person'
+import { PersonList } from './components/PersonList';
 
 function App() {
+  const personName = {
+    first: "Bruce",
+    last: "Wayne",
+  }
+
+  const nameList = [
+    {
+      first: "Bruce",
+      last: "Wayne"
+    },
+    {
+      first: "Clark",
+      last: "Kent"
+    },
+    {
+      first: "Princess",
+      last: "Diana"
+    },
+  ]
   return (
     <div className="App">
-      <Greet name="Toba" />
+      <Greet name={"Toba"} messageCount={45} isLoggedIn={false} />
+      <Person name={personName} />
+      <PersonList names={nameList} />
     </div>
   );
 }
