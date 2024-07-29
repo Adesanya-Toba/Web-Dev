b25a3a952ad9f1dce5fed2de79305d3a6c90ae8b
Toba-Adesanya <adesanyatoba@gmail.com>
2024-07-29 01:57:07 +0100

    Using the react hook form to manage form state

---
+++ React-TS/react-hook-form/notes/notes.md
@@ -0,0 +1,4 @@
+### Install React Hook Form
+With `npm install react-hook-form`. Observe the new dependency in package.json.
+
+- `...` : spread operator
--- React-TS/react-hook-form/src/components/youtubeform.tsx
+++ React-TS/react-hook-form/src/components/youtubeform.tsx
@@ -1,18 +1,25 @@
-import React from 'react'
+import { useForm } from 'react-hook-form'
 
 export const YouTubeForm = () => {
+    const form = useForm();
+    const { register } = form // This method allows us to register a form control with react hook form
+    const { name, ref, onChange, onBlur } = register("username") // This method returns 4 methods that we need to hook into the form control
+
     return (
         <div>
             <form>
                 <label htmlFor='username'>Username</label>
-                <input type='text' id='username' name='username' />
+                {/* Doing the below allows RHF to start tracking the state of the form.*/}
+                <input type='text' id='username' name={name} ref={ref} onChange={onChange} onBlur={onBlur} />
 
                 <label htmlFor='email'>Email</label>
-                <input type='email' id='email' name='email' />
+                {/* Or simply */}
+                <input type='email' id='email' {...register("email")} />
 
                 <label htmlFor='channel'>Channel</label>
-                <input type='text' id='channel' name='channel' />
+                <input type='text' id='channel' {...register("channel")} />
 
+                <button>Submit</button>
             </form>
         </div>
     )
