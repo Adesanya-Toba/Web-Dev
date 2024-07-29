56bf003ab11b65c6f5f4cc14621c1f80ceee365f
Toba-Adesanya <adesanyatoba@gmail.com>
2024-07-29 22:27:43 +0100

    Added some html to app.js

--- FullStack/React_FastAPI/React/finance-app/src/App.js
+++ FullStack/React_FastAPI/React/finance-app/src/App.js
@@ -21,5 +21,41 @@ const App = () => {
   useEffect(() => {
     fetchTransactions();
   }, [])
+
+  const handleInputChange = (event) => {
+    const value = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    setFormData({
+      ...formData,
+      [event.target.name]: value,
+    });
+  };
+
+  const handleFormSubmit = async (event) => {
+    event.preventDefault(); // Overriding the default submit behavior, i.e., you'd still see the data
+    await api.post("/transactions", formData);
+    fetchTransactions(); // Refresh front-end with latest data after submitting
+    // The line below resets the form fields with the default data.
+    setFormData({
+      amount: "",
+      category: "",
+      description: "",
+      is_income: false,
+      date: ""
+    });
+  };
+
+  // React can only return one element
+  return (
+    <div>
+      {/* Using bootstrap CSS */}
+      <nav className='navbar navbar-dark bg-primary'>
+        <div className='container-fluid'>
+          <a className='navbar-brand' href='#'>
+            Finance App
+          </a>
+        </div>
+      </nav>
+    </div>
+  )
 }
 export default App;
