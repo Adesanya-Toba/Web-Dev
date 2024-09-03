2f99ab847b80f7c8822c199dafe3864f71666b6a
Toba-Adesanya <adesanyatoba@gmail.com>
2024-09-04 00:19:37 +0100

    Adding and using props

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,3 +1,10 @@
+/** Understanding Destructing/Unpacking */
+/** Create a JS object */
+const obj = { obj_name: "Toba", job: "engineer" };
+const { obj_name } = obj; // unpacks just the object name from the obj
+
+console.log(obj_name);
+
 const Title = () => {
   return <div>Hello World</div>;
 };
@@ -7,11 +14,22 @@ function MySecondReactComponent() {
   return <div>My Second React Component!</div>;
 }
 
+const Message = (props) => {
+  const { text, bgColor } = props;
+  console.log(bgColor);
+
+  // Using the inputs from the Message to render the div using the builtin 'style' prop in the div.
+  return <div style={{ background: bgColor }}>{text}</div>;
+};
+
 const App = () => {
   return (
     <div>
       <Title />
       <MySecondReactComponent />
+      <Message text="this is my first prop" bgColor="Red" />
+      <Message text="hello" bgColor="White" />
+      <Message text="bye" bgColor="Blue" />
     </div>
   );
 };
