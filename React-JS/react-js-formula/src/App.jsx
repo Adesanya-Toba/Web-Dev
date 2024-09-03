b297b31915dbfc6770681b3b83bf3da16cab8145
Toba-Adesanya <adesanyatoba@gmail.com>
2024-09-03 23:43:44 +0100

    Adding js react components

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+const Title = () => {
+  return <div>Hello World</div>;
+};
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+// Using the 'function' method to create components
+function MySecondReactComponent() {
+  return <div>My Second React Component!</div>;
 }
 
-export default App
+const App = () => {
+  return (
+    <div>
+      <Title />
+      <MySecondReactComponent />
+    </div>
+  );
+};
+
+export default App;
