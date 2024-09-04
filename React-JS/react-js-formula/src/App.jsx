9827ce1eeb5eea47e4642fbc2090835b634c96c8
Toba <adesanyatoba@gmail.com>
2024-09-04 12:28:11 +0100

    Using boolean props

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -3,7 +3,7 @@
 const obj = { obj_name: "Toba", job: "engineer" };
 const { obj_name } = obj; // unpacks just the object name from the obj
 
-console.log(obj_name);
+// console.log(obj_name);
 
 const Title = () => {
   return <div>Hello World</div>;
@@ -19,17 +19,46 @@ const Message = (props) => {
   console.log(bgColor);
 
   // Using the inputs from the Message to render the div using the builtin 'style' prop in the div.
+  // Outside curly brace is , inside curly brace is to contain the js objects
   return <div style={{ background: bgColor }}>{text}</div>;
+};
+
+// Using boolea values in props
+const Box = (props) => {
+  const { label, textColor, bgColor, large } = props;
+  console.log(large);
+
+  return (
+    <div
+      style={{
+        color: textColor,
+        background: bgColor,
+        fontSize: large ? "40px" : "18px",
+      }}
+    >
+      {label}
+    </div>
+  );
 };
 
 const App = () => {
   return (
     <div>
-      <Title />
+      {/* <Title />
       <MySecondReactComponent />
       <Message text="this is my first prop" bgColor="Red" />
       <Message text="hello" bgColor="White" />
-      <Message text="bye" bgColor="Blue" />
+      <Message text="bye" bgColor="Blue" /> */}
+      <Box
+        label="time to review"
+        textColor="Grey"
+        bgColor="skyblue"
+        large={true}
+      />{" "}
+      {/*Curly brace to evaluate the JS*/}
+      <Box label="hello" textColor="Purple" bgColor="Lavender" large={false} />
+      <Box label="green stuff" textColor="white" bgColor="green" />{" "}
+      {/*Large property is not passed so it acts as undefined. */}
     </div>
   );
 };
