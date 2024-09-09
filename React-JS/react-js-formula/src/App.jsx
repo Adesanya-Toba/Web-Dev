05af702a886ee279101fa21bea42832cc0087129
Toba <adesanyatoba@gmail.com>
2024-09-09 10:59:59 +0100

    dynamic class names in react

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,15 +1,12 @@
 const App = () => {
+  const boolean = false;
+  const color = "green";
+  // return <div className={`bg-${boolean ? "orange" : "purple"}-600`}>todo</div>;
   return (
-    <div>
-      <div className="text-rose-200 bg-red-600">one</div>
-      <div className="text-slate-400 border-y-4 border-black">two</div>
-      <div className="text-3xl text-slate-400 bg-emerald-300 text-blue-600">
-        three
-      </div>
-      <div className="text-xl text-purple-500">
-        <div className="border-4 border-sky-500">four</div>
-        <div className="bg-violet-200 border-l-8 border-violet-800">five</div>
-      </div>
+    <div
+      className={`${boolean ? "bg-orange-600" : "bg-purple-600"} text-white`}
+    >
+      todo
     </div>
   );
 };
