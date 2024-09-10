b74da72bced5dc87873e46dfdf91f8a2f7c8da90
Toba <adesanyatoba@gmail.com>
2024-09-10 11:55:46 +0100

    Archive flexin cats

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,35 +1,5 @@
-import Cat from "./components/Cat";
-
 const App = () => {
-  return (
-    <div>
-      <div className="flex justify-center h-64 p-4 m-4 border-2 border-blue-400">
-        <Cat />
-        <Cat />
-        <Cat />
-      </div>
-      <div className="flex justify-between h-64 p-4 m-4 border-2 border-red-400">
-        <Cat />
-        <Cat />
-        <Cat />
-      </div>
-      <div className="flex justify-center items-center h-64 p-4 m-4 border-2 border-green-400">
-        <Cat />
-        <Cat />
-        <Cat />
-      </div>
-      <div className="flex justify-around items-end h-64 p-4 m-4 border-2 border-purple-400">
-        <Cat />
-        <Cat />
-        <Cat />
-      </div>
-      <div className="flex justify-between items-center h-64 p-4 m-4 border-2 border-orange-400">
-        <Cat />
-        <Cat />
-        <Cat />
-      </div>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
