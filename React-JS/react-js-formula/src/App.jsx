f5c8adad9e898cde24b272ce395f5f0c81bd2b8e
Toba <adesanyatoba@gmail.com>
2024-09-10 11:51:19 +0100

    Flex lessons

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,9 +1,33 @@
-import FancyButton from "./components/FancyButton";
+import Cat from "./components/Cat";
+
 const App = () => {
   return (
     <div>
-      <FancyButton>Click Me</FancyButton>
-      <FancyButton large={true}>Submit</FancyButton>
+      <div className="flex justify-center h-64 p-4 m-4 border-2 border-blue-400">
+        <Cat />
+        <Cat />
+        <Cat />
+      </div>
+      <div className="flex justify-between h-64 p-4 m-4 border-2 border-red-400">
+        <Cat />
+        <Cat />
+        <Cat />
+      </div>
+      <div className="flex justify-center items-center h-64 p-4 m-4 border-2 border-green-400">
+        <Cat />
+        <Cat />
+        <Cat />
+      </div>
+      <div className="flex justify-around items-end h-64 p-4 m-4 border-2 border-purple-400">
+        <Cat />
+        <Cat />
+        <Cat />
+      </div>
+      <div className="flex justify-between items-center h-64 p-4 m-4 border-2 border-orange-400">
+        <Cat />
+        <Cat />
+        <Cat />
+      </div>
     </div>
   );
 };
---
+++ React-JS/react-js-formula/src/components/Cat.jsx
@@ -0,0 +1,9 @@
+const Cat = () => {
+  return (
+    <div className="p-8 border-2 rounded-lg border-amber-400">
+      <i className="fa-solid fa-cat text-7xl text-amber-400"></i>
+    </div>
+  );
+};
+
+export default Cat;
