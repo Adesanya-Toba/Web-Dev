0343f0812e23e4cc876d91d330cbd27db08277a8
Toba <adesanyatoba@gmail.com>
2024-09-18 14:22:37 +0100

    Widget 3

--- React-JS/react-js-formula/notes.md
+++ React-JS/react-js-formula/notes.md
@@ -10,3 +10,15 @@
 ### Hooks
 
 Hooks are functions in react that help to achieve some additional functionality inside our component.
+
+### Logic AND Operator
+
+The below will render only when `showIcon` is true.
+
+```js
+{
+  showIcon && (
+    <i className="fa-solid fa-poop text-fuchsia-500 text-3xl mb-4"></i>
+  );
+}
+```
---
+++ React-JS/react-js-formula/src/components/Widget3.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+
+const Widget3 = () => {
+  const [showIcon, setShowIcon] = useState(false);
+  return (
+    <div className="bg-purple-100 border border-purple-300 p-4 rounded-lg m-4 flex flex-col items-center">
+      {showIcon && (
+        <i className="fa-solid fa-poop text-fuchsia-500 text-3xl mb-4"></i>
+      )}
+
+      <button
+        className="px-4 py-2 bg-fuchsia-500 text-white rounded-full"
+        onClick={() => setShowIcon(!showIcon)}
+      >
+        click me!
+      </button>
+    </div>
+  );
+};
+
+export default Widget3;
