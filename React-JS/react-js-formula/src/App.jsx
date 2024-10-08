467a1d4abfdb1d0c1106355b1001b7e1c20b20b1
Toba <adesanyatoba@gmail.com>
2024-10-08 12:09:46 +0100

    archive map exercise

--- React-JS/react-js-formula/src/App.jsx
+++ React-JS/react-js-formula/src/App.jsx
@@ -1,18 +1,5 @@
-import messages from "./components/messages";
-
 const App = () => {
-  // Implicitly returning a div without the 'return' keyword
-  const messageItems = messages.map((msg, idx) => (
-    <div key={idx} className="flex border border-zinc-300 rounded-lg m-4 p-4">
-      <div className="font-bold mr-2">{msg.user}:</div>
-      {msg.text}
-    </div>
-  ));
-  return (
-    <div className="flex justify-center">
-      <div className="w-full max-w-xl">{messageItems}</div>
-    </div>
-  );
+  return <div>todo</div>;
 };
 
 export default App;
