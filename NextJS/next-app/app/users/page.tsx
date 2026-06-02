db22abc47ee87005c65fc09e54bdec0b10dc2d29
Toba <oluwatoba.adesanya@rotork.com>
2026-06-02 10:46:31 +0100

    feat: Create UserTable component

--- NextJS/next-app/app/users/page.tsx
+++ NextJS/next-app/app/users/page.tsx
@@ -1,33 +1,11 @@
 import React from "react";
-
-interface User {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const users: User[] = await res.json();
   return (
     <div>
       <h1>Users</h1>
-      <table className="table table-bordered">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </div>
   );
 };
